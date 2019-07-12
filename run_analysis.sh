11fceae59901561cffe1f7deb878936eaaf949c9
Will Usher <william.usher@ouce.ox.ac.uk>
2019-07-12 16:07:10 +0100

    Add gurobi commands to script

--- run_analysis.sh
+++ run_analysis.sh
@@ -8,17 +8,11 @@ rm -f mycplexcommands
 touch mycplexcommands
 
 echo "read output_data/temba.lp" > mycplexcommands
-echo "optimize"             >> mycplexcommands
-echo "write"                >> mycplexcommands
-echo "output_data/temba.sol"    >> mycplexcommands
-echo "quit"                 >> mycplexcommands
-
-# Executes the cplex script written above
-# Should set the path to CPLEX
+echo "optimize" >> mycplexcommands
+echo "write" >> mycplexcommands
+echo "output_data/temba.sol" >> mycplexcommands
+echo "quit" >> mycplexcommands
 cplex < mycplexcommands
-
-# Solve with CBC
-cbc temba.lp solve -solu output_data/solutionfile.txt
 
 python scripts/transform_31072013.py output_data/temba.sol output_data/temba_solution.txt
 sort output_data/temba_solution.txt -o output_data/temba_sorted.txt
@@ -26,3 +20,10 @@ sort output_data/temba_solution.txt -o output_data/temba_sorted.txt
 wget https://raw.githubusercontent.com/OSeMOSYS/OSeMOSYS_GNU_MathProg/cplex_to_cbc/scripts/convert_cplex_to_cbc.py -O scripts/convert_cplex_to_cbc.py
 
 python scripts/convert_cplex_to_cbc.py output_data/temba_sorted.txt output_data/temba_sorted_cbc.txt
+
+# Solve with CBC
+cbc output_data/temba.lp solve -solu output_data/temba_sorted_cbc.txt
+
+# Solve with Gurobi
+gurobi_cl NumericFocus=1 ResultFile=output_data/temba_gur.sol LogFile=output_data/temba_gur.log output_data/temba.lp
+sed '/ * 0/d' output_data/temba_gur.sol >> output_data/temba_gur_nz.sol # Remove zero value items
